637bab563467b8af961c23eba92290809f90eb46
Zie619 <54673680+Zie619@users.noreply.github.com>
2020-11-16 12:44:31 +0200

    Update Frozen-lake.py

--- Frozen-lake.py
+++ Frozen-lake.py
@@ -12,14 +12,16 @@ import matplotlib.pyplot as plt
 
 #initilize Q and setting the env:
 env=gym.make('FrozenLake-v0')
-Q = np.ones((env.observation_space.n, env.action_space.n))
+Q = np.zeros((env.observation_space.n, env.action_space.n))
 
 #initizlize hyper-parameters:
-learning_rate = 0.9
-discount_factor = 0.9
-epsilon = 0.6
-number_of_episodes = 700
+learning_rate = 0.91
+discount_factor = 0.83
+epsilon = 0.76
+number_of_episodes = 5000
 max_steps=100
+epsilon_discount_factor=0.993
+epsilon_decay=400 
 Q_at_range=[]
 reward_per_episode=[]
 steps_at_episode=[]
@@ -43,7 +45,7 @@ def learn(state, action, state2, reward):
 #The Q-learning algorithm:
     '''
     state2 is the next state for Q[state,action]
-    reward - reward returned from the state2 [-1,0,1]
+    reward - reward returned from the state2 [0,1]
     fall- if the player died or managed to go to the next step ; 1 for go , 0 for dead.
     info- info for debugging
     '''
@@ -55,23 +57,17 @@ for k in range(number_of_episodes):
         action = choose_action(state, epsilon)
         state2, reward, done, info = env.step(action)
         learn(state, action, state2, reward)
-        epsilon=0.9*epsilon
+        if k>epsilon_decay:
+            epsilon=epsilon_discount_factor*epsilon
         state = state2
         if done:
             break
-        time.sleep(0.1)
-    # printing the values - remove later:
-    print(k)
-    print(t)
-    print(reward)
-    print(Q)
-
     # appending k to a list for later use, if fall steps=100 if not save the steps
     if reward == 1:
-        steps_at_episode.append(k)
+        steps_at_episode.append(t)
     if reward == 0:
         steps_at_episode.append(100)
-    if k == 500 or k == 2000 or k == number_of_episodes:  #append Q at specific k
+    if k == 500 or k == 2000 or k == number_of_episodes-1:  #append Q at specific k
         print("yay middle")
         Q_at_range.append(Q)
     reward_per_episode.append(reward)
@@ -90,6 +86,9 @@ Hyper-parameters values that were used for the final solution.
 every 100 episodes). If agent didn't get to the goal, the number of steps of
 the episode will be set to 100. 
 '''
+steps_at_episode=np.add.reduceat(steps_at_episode, np.arange(0, len(steps_at_episode), 100))
+steps_at_episode=steps_at_episode/100
+
 # reward vs episode graph:
 x = list_for_graphs
 y = reward_per_episode
@@ -100,10 +99,24 @@ plt.title('Reward per episode')
 plt.show()
 
 
-x = steps_at_episode
-y = (0, 5000, 100)
+y = steps_at_episode
+x = range(0,number_of_episodes,100)
 plt.plot(x, y)
 plt.xlabel('Number of episodes ')
-plt.ylabel('reward')
-plt.title('Reward per episode')
+plt.ylabel('sum of steps')
+plt.title('steps per episode')
 plt.show()
+
+for i in  (Q_at_range):
+    fig = plt.figure(figsize=(160,40))
+    ax = fig.add_subplot( frameon=False, xticks = [], yticks = [])
+    cells = i
+    img = plt.imshow(cells, cmap="Accent")
+    img.set_visible(False)
+    tb = plt.table(cellText = cells, 
+        rowLabels = range(16), 
+        colLabels = range(4), 
+        loc = 'center',
+        cellColours = img.to_rgba(cells))
+    ax.add_table(tb)
+    plt.show()
